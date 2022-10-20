68596c71f2d3c71bd15f538292c3a78463918e3e
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2022-10-20 11:17:29 +0100

    routing

--- routing/routes/web.php
+++ routing/routes/web.php
@@ -16,3 +16,49 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Basic Route
+Route::get('/index', function () {
+    return 'Hello World';
+});
+
+//controller route
+
+Route::get('/hello', [Controller::class, 'index']);
+
+
+
+// route with view
+
+Route::get('/request', function () {
+    return view('request');
+});
+
+
+// route with request
+ 
+Route::any('/user', function (Request $request) {
+    $name = $request->input("name");
+    return $name;
+});
+
+
+// route with 2 method
+Route::match(['get', 'post'], '/', function () {
+});
+ 
+
+// route any method
+Route::any('/', function () {
+});
+
+// redirect route
+Route::redirect('/her', 'hello');
+
+
+
+// route with id
+Route::get('/person/{id}', function ($id) {
+    return 'person id : '.$id;
+});
+
